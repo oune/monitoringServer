3d11a8c3b2cd77cd947b1213210a9c8a9b4db4b9
Junjae Lee <ejj612o@gmail.com>
2022-11-24 16:41:21 +0900

    config 파일 개선 시도

--- config.py
+++ config.py
@@ -1,35 +1,55 @@
 import configparser
 
-config = configparser.ConfigParser()
+
+class Serializable:
+    def __init__(self, name, filename, config):
+        self._name: str = name
+        self._filename = filename
+        self._config = config
+
+    def save(self):
+        self._config[self._name] = {}
+
+        for (key, value) in self.__dict__.items():
+            if not str(key).startswith('_'):
+                self._config[key] = value
+
+        with open(self._filename, 'w') as configfile:
+            self._config.write(configfile)
 
 
-def write(file_name: str):
-    config['model'] = {}
-    config['model']['device'] = 'cDAQ1Mod1'
-    config['model']['channels'] = 'ai0:3'
-    config['model']['sampling_rate'] = '51200'
-    config['model']['samples_per_channel'] = '51200'
-    config['model']['type'] = 'vib'
-    config['model']['ip'] = '127.0.0.1'
-    config['model']['port'] = '8000'
-
-    with open(file_name, 'w') as configfile:
-        config.write(configfile)
+class Sensor(Serializable):
+    def __init__(self, name, filename, config, device, channels, sampling_rate, samples_per_channel, model_type):
+        super().__init__(name, filename, config)
+        self.device = device
+        self.channels = channels
+        self.sampling_rate = sampling_rate
+        self.samples_per_channel = samples_per_channel
+        self.model_type = model_type
 
 
-def load(file_name: str):
-    config.read(file_name)
-    device_name = config['model']['device']
-    device_channel_name = config['model']['channels']
-    sampling_rate = int(config['model']['sampling_rate'])
-    samples_per_channel = int(config['model']['samples_per_channel'])
-    model_type = config['model']['type']
-    ip = config['model']['ip']
-    port = int(config['model']['port'])
+class Server(Serializable):
+    def __init__(self, name, filename, config, host_ip: str, port: int, database: str):
+        super().__init__(name, filename, config)
+        self.host_ip: str = host_ip
+        self.port: int = port
+        self.database: str = database
 
-    return device_name, device_channel_name, sampling_rate, samples_per_channel, model_type, ip, port
+
+class Config:
+    def __init__(self, filename: str, config, temp: Sensor, vib: Sensor, server: Server):
+        self.filename = filename
+        self.temp: Sensor = temp
+        self.vib: Sensor = vib
+        self.server: Server = server
 
 
 if __name__ == '__main__':
-    import sys
-    write(sys.argv[1])
+    configparser = configparser.ConfigParser()
+    config_file_name = 'config_t.ini'
+    sensor_temp = Sensor('temp', config_file_name, configparser, 'Dev0mod1', 'ai0:3', 10, 20, 'temp')
+    sensor_vib = Sensor('vib', config_file_name, configparser, 'Dev0mod1', 'ai0:3', 10, 20, 'vib')
+    server_config = Server('server', config_file_name, configparser, )
+
+    print(sensor_temp.__dict__)
+    print(sensor_vib.__dict__)
