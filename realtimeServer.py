017ef3a1908b9a672c7518e5fb1e9b8742ca3a31
Junjae Lee <ejj612o@gmail.com>
2022-11-30 14:47:43 +0900

    config 분리

--- realtimeServer.py
+++ realtimeServer.py
@@ -1,8 +1,8 @@
 from uvicorn import Config, Server
 from sensor import Sensor
 from sys import exit
+from configparser import ConfigParser
 
-import configparser
 import socketio
 import nidaqmx
 import asyncio
@@ -10,9 +10,7 @@ import asyncio
 sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins='*')
 
 
-def config_load(file_name: str):
-    config = configparser.ConfigParser()
-    config.read(file_name)
+def config_load(config: ConfigParser):
     sampling_rate = int(config['sensor']['rate'])
     sensor_buffer_size = sampling_rate * 2
 
@@ -25,16 +23,24 @@ def config_load(file_name: str):
     return sampling_rate, sensor_buffer_size, vib_channel_name, temp_channel_name
 
 
-try:
-    rate, buffer_size, vib_channel, temp_channel = config_load('config.ini')
-    sensor_vib = Sensor.vib(vib_channel, rate, buffer_size)
-    sensor_vib.set_sample_count(rate)
-    sensor_temp = Sensor.temp(temp_channel, rate, buffer_size)
-    sensor_temp.set_sample_count(rate)
+def init_sensor(sampling_rate: int, sensor_buffer_size: int, vib_channel_name: str, temp_channel_name: str):
+    vib = Sensor.vib(vib_channel_name, sampling_rate, sensor_buffer_size)
+    temp = Sensor.temp(temp_channel_name, sampling_rate, sensor_buffer_size)
+    return vib, temp
 
-except nidaqmx.errors.DaqError:
-    print('잘못된 설정값이 입력 되었습니다. config.ini 파일을 올바르게 수정해 주세요.')
-    exit()
+
+def try_sensor_load(config: ConfigParser):
+    rate, buffer_size, vib_channel, temp_channel = config_load(config)
+    vib, temp = init_sensor(rate, buffer_size, vib_channel, temp_channel)
+    return vib, temp
+
+
+def sensor_load(config: ConfigParser):
+    try:
+        return try_sensor_load(config)
+    except nidaqmx.errors.DaqError:
+        print('잘못된 설정값이 입력 되었습니다. config.ini 파일을 올바르게 수정해 주세요.')
+        exit()
 
 
 async def sensor_loop():
@@ -50,18 +56,22 @@ async def sensor_loop_vib():
 
 async def sensor_loop_temp():
     while True:
-        await sensor_vib.read(sio, 'temp')
+        await sensor_temp.read(sio, 'temp')
 
 
 if __name__ == "__main__":
+    conf = ConfigParser()
+    conf.read('config.ini')
+    sensor_vib, sensor_temp = sensor_load(conf)
+
     socket_app = socketio.ASGIApp(sio)
     sensor_task_vib = sio.start_background_task(sensor_loop_vib)
     sensor_task_temp = sio.start_background_task(sensor_loop_temp)
     main_loop = asyncio.get_event_loop()
 
     socket_config = Config(app=socket_app,
-                           host=config['server']['ip'],
-                           port=int(config['server']['port']),
+                           host=conf['server']['ip'],
+                           port=int(conf['server']['port']),
                            loop=main_loop)
     socket_server = Server(socket_config)
 
