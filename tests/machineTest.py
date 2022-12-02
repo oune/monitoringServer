98a8193ec85c8114eeb96d4d8573e9bc015ca09e
Junjae Lee <ejj612o@gmail.com>
2022-12-02 16:31:28 +0900

    테스트 코드 추가

---
+++ tests/machineTest.py
@@ -0,0 +1,51 @@
+import unittest
+from dataController import Machine
+
+
+class MyTestCase(unittest.TestCase):
+    def test_batch_size_3(self):
+        def do_pass():
+            pass
+
+        machine = Machine(do_pass, 3)
+        machine.add_vib_left([1, 2, 3])
+        self.assertEqual(machine.vib_left, [1, 2, 3])
+        machine.add_vib_right([1, 2, 3])
+        self.assertEqual(machine.vib_right, [1, 2, 3])
+        machine.add_temp([1, 2, 3])
+        self.assertEqual(machine.temp, [])
+        self.assertEqual(machine.vib_left, [])
+        self.assertEqual(machine.vib_right, [])
+
+    def test_batch_size_10(self):
+        def do_pass():
+            pass
+
+        machine = Machine(do_pass, 10)
+        machine.add_vib_left([1, 2, 3])
+        self.assertEqual(machine.vib_left, [1, 2, 3])
+        machine.add_vib_right([1, 2, 3])
+        self.assertEqual(machine.vib_right, [1, 2, 3])
+        machine.add_temp([1, 2, 3])
+
+        self.assertEqual(machine.temp, [1, 2, 3])
+        self.assertEqual(machine.vib_left, [1, 2, 3])
+        self.assertEqual(machine.vib_right, [1, 2, 3])
+
+        machine.add_vib_left([1, 2, 3])
+        machine.add_vib_right([1, 2, 3])
+        machine.add_temp([1, 2, 3])
+        machine.add_vib_left([1, 2, 3])
+        machine.add_vib_right([1, 2, 3])
+        machine.add_temp([1, 2, 3])
+        machine.add_vib_left([1, 9, 11])
+        machine.add_vib_right([1, 9, 11])
+        machine.add_temp([1, 9, 11])
+
+        self.assertEqual(machine.temp, [9, 11])
+        self.assertEqual(machine.vib_left, [9, 11])
+        self.assertEqual(machine.vib_right, [9, 11])
+
+
+if __name__ == '__main__':
+    unittest.main()
