cb6f06ac7f0d90bec4a77156d6e0306dc33f9ce9
Junjae Lee <ejj612o@gmail.com>
2022-12-19 17:29:19 +0900

    코드 리팩토링

--- filesystem.py
+++ filesystem.py
@@ -5,8 +5,8 @@ from clock import TimeController
 class Saver:
     def __init__(self, directory_path: str, file_name: str):
         self.directory_path = directory_path
-        self.time = TimeController()
         self.file_name = file_name
+        self.time = TimeController()
 
     def save(self, data):
         pass
