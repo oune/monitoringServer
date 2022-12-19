807513c696b54b97529c07579f818039f0223228
Junjae Lee <ejj612o@gmail.com>
2022-12-19 17:52:26 +0900

    테스트 코트 비동기 적용

--- tests/databaseTest.py
+++ tests/databaseTest.py
@@ -1,14 +1,18 @@
 import unittest
+from unittest import IsolatedAsyncioTestCase
 from db import Database
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
+class MyTestCase(IsolatedAsyncioTestCase):
+    async def test_read(self):
         db = Database("../db/machine_1.db")
-        print(db.get_all())
-        print(db.get_by_one_day('2022-12-19'))
-        print(db.get_by_duration('2022-12-13', '2022-12-20'))
+        print(await db.get_all())
+        print(await db.get_by_one_day('2022-12-19'))
+        print(await db.get_by_duration('2022-12-13', '2022-12-20'))
 
+    async def test_save_now(self):
+        db = Database("../db/machine_1.db")
+        await db.save_now(3.1111)
 
 if __name__ == '__main__':
     unittest.main()
