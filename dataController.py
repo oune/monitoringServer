aae202047b43a7e0a74aa40bab20920b19a8cfb4
Junjae Lee <ejj612o@gmail.com>
2022-12-20 15:06:17 +0900

    type hint 비동기 타입으로 변경

--- dataController.py
+++ dataController.py
@@ -1,18 +1,22 @@
-from typing import Callable, List
+from typing import Callable, List, Awaitable
 
 
 class Machine:
-    def __init__(self, name: str, callback: Callable[[List[float], List[float], List[float], str], None], batch_size: int = 10):
+    def __init__(self, name: str,
+                 model_callback: Callable[[List[float], List[float], List[float], str], Awaitable[None]],
+                 batch_size: int = 10):
         self.vib_left = []
         self.vib_right = []
         self.temp = []
         self.batch_size = batch_size
-        self.callback = callback
+        self.model_callback = model_callback
         self.name = name
 
-    async def trigger(self):
+    async def model_trigger(self):
         if self.is_batch():
-            await self.callback(self.vib_left[:self.batch_size], self.vib_right[:self.batch_size], self.temp[:self.batch_size], self.name)
+            await self.model_callback(self.vib_left[:self.batch_size], self.vib_right[:self.batch_size],
+                                      self.temp[:self.batch_size], self.name)
+
             self.clear_batch()
 
     def is_batch(self):
@@ -34,15 +38,15 @@ class Machine:
     async def add_vib(self, data_list):
         self.add_vib_left(data_list[0])
         self.add_vib_right(data_list[1])
-        await self.trigger()
+        await self.model_trigger()
 
     async def add_temp(self, data):
         self.temp.extend(data)
-        await self.trigger()
+        await self.model_trigger()
 
 
 class DataController:
-    def __init__(self, model_req: Callable[[List[float], List[float], List[float]], None], batch_size):
+    def __init__(self, model_req: Callable[[List[float], List[float], List[float]], Awaitable[None]], batch_size):
         self.machine1 = Machine('machine1', model_req, batch_size)
         self.machine2 = Machine('machine2', model_req, batch_size)
 
