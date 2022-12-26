40ba87ab0436487a2567e6826afb377e4f3ef9fd
Junjae Lee <ejj612o@gmail.com>
2022-12-26 12:32:33 +0900

    테스트 비동기로 수정

--- tests/csvWriterTest.py
+++ tests/csvWriterTest.py
@@ -2,9 +2,10 @@ import unittest
 import csv
 from csvwriter import CsvWriter
 from freezegun import freeze_time
+from unittest import IsolatedAsyncioTestCase
 
 
-class MyTestCase(unittest.TestCase):
+class MyTestCase(IsolatedAsyncioTestCase):
     def test_writer_init(self):
         cs = CsvWriter('data', 'temp', ['123', '223', '323'])
         print(cs.get_path())
@@ -21,30 +22,30 @@ class MyTestCase(unittest.TestCase):
         data_list = [[0.12 + j for j in range(0, 10)] for i in range(0, 4)]
         cs.save(data_list)
 
-    def test_save_day_pass(self):
+    async def test_save_day_pass(self):
         cs = CsvWriter('data', 'temp3', ['no.1', 'no.2', 'no.3', 'no.4'])
         data_list = [[0.12 + j for j in range(0, 10)] for i in range(0, 4)]
 
         with freeze_time("2012-01-14 14:59:59"):
-            cs.save(data_list)
+            await cs.save(data_list)
 
         with freeze_time("2012-01-14 15:00:00"):
-            cs.save(data_list)
+            await cs.save(data_list)
 
         with freeze_time("2012-01-14 15:00:01"):
-            cs.save(data_list)
+            await cs.save(data_list)
 
         with freeze_time("2012-01-14 16:00:00"):
-            cs.save(data_list)
+            await cs.save(data_list)
 
         with freeze_time("2012-01-14 17:00:00"):
-            cs.save(data_list)
+            await cs.save(data_list)
 
         with freeze_time("2012-01-14 18:00:00"):
-            cs.save(data_list)
+            await cs.save(data_list)
 
         with freeze_time("2012-01-14 19:00:00"):
-            cs.save(data_list)
+            await cs.save(data_list)
 
 
 
